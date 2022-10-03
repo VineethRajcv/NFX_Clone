a9f25d2e730dd0a7b39cc7ba761627fc14e4becd
vineeth <vineethrajchirakkal@gmail.com>
2022-10-03 19:53:31 +0530

    UI first commit

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie Name</h1>
+            <div className='banner_buttons'>
+                <button className='button'>Play</button>
+                <button className='button'>My List</button>
+            </div>
+            <h1 className='description'> Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan. Watch all you want. This riveting crime series won Best Drama at the International Emmy Awards, Premios Fénix and Premios Iris (plus six more Iris wins). </h1>
+        </div>
+    <div className="fade_bottom"></div>   
+
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./NavBar.css"
+
+function NavBar() {
+  return (
+    <div className="navbar">
+        <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="NetflixLogo" />
+        <img className='avatar' src="https://media-exp1.licdn.com/dms/image/C4D03AQHLIRYZaE0RNw/profile-displayphoto-shrink_100_100/0/1663955974928?e=1670457600&v=beta&t=jwRgU4KGc0a00_pXOThA3u1LZVo4ape7kBp4zukwXFw" alt="Avatar" />
+
+
+    </div>
+  )
+}
+
+export default NavBar
