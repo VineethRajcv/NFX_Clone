3811b79c798cf590e8c15ddc91bbea20a392955e
vineeth <vineethrajchirakkal@gmail.com>
2022-10-10 22:56:38 +0530

    Banner completed

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import axios from '../../axios'
+import {API_KEY,imageUrl} from '../../Constatnts/Constants'
+
+
 
 function Banner() {
+    const [movie, setMovie] = useState()
+    useEffect(() => {
+
+        axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+            const r=Math.floor(Math.random() * (19 + 1));
+            console.log(response.data.results[r])
+            setMovie(response.data.results[r])
+        })
+    
+    }, [])
+    
   return (
-    <div className='banner'>
+    <div style ={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""})`}}className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+            <h1 className='title'>{movie ? movie.title:""}</h1>
             <div className='banner_buttons'>
                 <button className='button'>Play</button>
                 <button className='button'>My List</button>
             </div>
-            <h1 className='description'> Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan. Watch all you want. This riveting crime series won Best Drama at the International Emmy Awards, Premios Fénix and Premios Iris (plus six more Iris wins). </h1>
+            <h1 className='description'> {movie ? movie.overview:""} </h1>
         </div>
     <div className="fade_bottom"></div>   
 
